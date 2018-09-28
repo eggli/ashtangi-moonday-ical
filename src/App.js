dc8689c6b69631acae3ad69f826b97ad6d37ef39
Lee Cheng Wei <aeggli@gmail.com>
2018-09-28 13:26:26 +0800

    add lune.js, upgrade bootstrap, add github page deploy scripts

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import lune from 'lune';
+
+const CalendarTypes = {
+  NASA: 'nasa'
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timezone: new Date().getTimezoneOffset(),
+      avoidPeakTime: true,
+      upToYear: 2200,
+      calendarType: CalendarTypes.NASA,
+      reminderOptions: {
+        dateOffset: -1,
+        timeToReminder: '18:00'
+      }
+    };
+  }
+
+  getMoonPhases = phaseType => {
+    const now = new Date();
+    const upTo = new Date(this.state.upToYear + '-12-31T23:59:59.999Z');
+
+    const phaseList = lune.phase_range(now, upTo, phaseType);
+    return phaseList;
+  };
+
+  getFullMoons = () => {
+    return this.getMoonPhases(lune.PHASE_FULL);
+  };
+
+  getNewMoons = () => {
+    return this.getMoonPhases(lune.PHASE_NEW);
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div />;
   }
 }
 
