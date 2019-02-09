68d480ef54928e485e21497bc7e4ad155e0ba5aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-09 07:42:52 +0000

    Deploy eggli/ashtangi-moonday-ical to github.com/eggli/ashtangi-moonday-ical.git:gh-pages

---
+++ precache-manifest.0fdf77da4891eb6fa1677b74f6b94127.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85218ccd973de43f8469",
+    "url": "/ashtangi-moonday-ical/static/css/main.f879fd84.chunk.css"
+  },
+  {
+    "revision": "85218ccd973de43f8469",
+    "url": "/ashtangi-moonday-ical/static/js/main.85218ccd.chunk.js"
+  },
+  {
+    "revision": "bba9f6208cd78ed1bacf",
+    "url": "/ashtangi-moonday-ical/static/css/1.18e5e035.chunk.css"
+  },
+  {
+    "revision": "bba9f6208cd78ed1bacf",
+    "url": "/ashtangi-moonday-ical/static/js/1.bba9f620.chunk.js"
+  },
+  {
+    "revision": "597f1dbab5d6b20a9999",
+    "url": "/ashtangi-moonday-ical/static/js/runtime~main.597f1dba.js"
+  },
+  {
+    "revision": "cd204d9091d35a87f61a40a2b6da61e6",
+    "url": "/ashtangi-moonday-ical/index.html"
+  }
+];
